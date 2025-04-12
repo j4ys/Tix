a3b89d68aba3e5d0f8fd9a9f11093c81441dda5e
j4ys <j4y1302@gmail.com>
2025-04-12 19:16:03 +0530

    Update dependencies, enhance TypeScript configuration, and refactor App component to use LoginForm. Integrate ThemeProvider in main.tsx for theme management.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "@/App";
+import { ThemeProvider } from "@/providers/themeProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
